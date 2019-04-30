835fc6303189e084345fc68a0725a48bab437e78
Zaine Kingi <T826739@spark.co.nz>
2019-04-30 14:45:45 +1200

    -Add Employee domain object class and move PayrollApplication file into Payroll folder.

---
+++ nonrest/src/main/java/payroll/Employee.java
@@ -0,0 +1,38 @@
+package payroll;
+
+/**
+ * @Data:
+ * Lombok annotation to create all the getters, setters,
+ * equals, hash, and toString methods, based on the fields.
+ */
+import lombok.Data;
+
+/**
+ * @Entity:
+ * JPA annotation to make this object ready for storage in a
+ * JPA-based data store.
+ */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+class Employee {
+
+  private @Id @GeneratedValue Long id;
+  private String name;
+  private String role;
+
+  // Empty constructor is required.
+  Employee() {}
+
+  /**
+   * custom constructor is created when we need to
+   * create a new instance, but don’t yet have an id.
+   */
+  Employee(String name, String role) {
+    this.name = name;
+    this.role = role;
+  }
+}
