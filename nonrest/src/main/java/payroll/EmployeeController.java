4b954840075e9c25c0850d3e8f870b8ae23aa249
Zaine Kingi <T826739@spark.co.nz>
2019-05-01 09:27:02 +1200

    -Add all CRUD operations to the EmployeeController class.

--- nonrest/src/main/java/payroll/EmployeeController.java
+++ nonrest/src/main/java/payroll/EmployeeController.java
@@ -19,7 +19,7 @@ class EmployeeController {
     this.repository = repository;
   }
 
-  // Aggregate root
+  // Get all Employees
   @GetMapping("/employees")
   List<Employee> all() {
     return repository.findAll();
@@ -31,4 +31,32 @@ class EmployeeController {
     return repository.findById(id)
       .orElseThrow(() -> new EmployeeNotFoundException(id));
   }
+
+  // Add new Employee
+  @PostMapping("/employees")
+  Employee newEmployee(@RequestBody Employee newEmployee) {
+    return repository.save(newEmployee);
+  }
+
+  // Update Employee record
+  @PutMapping("/employees/{id}")
+  Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+    return repository.findById(id)
+      .map(employee -> {
+        employee.setName(newEmployee.getName());
+        employee.setRole(newEmployee.getRole());
+        return repository.save(employee);
+      })
+      .orElseGet(() -> {
+        newEmployee.setId(id);
+        return repository.save(newEmployee);
+      });
+  }
+
+  // Delete Employee record
+  @DeleteMapping("/employees/{id}")
+  void deleteEmployee(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
+
 }
