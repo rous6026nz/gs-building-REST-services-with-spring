183d43a0db9fd22921705b70dbca98ae1355b3f8
Zaine Kingi <T826739@spark.co.nz>
2019-05-02 11:41:33 +1200

    -Revert master branch back to commit c5f0214 and update readme file.

--- nonrest/src/test/java/payroll/PayrollApplicationTests.java
+++ nonrest/src/test/java/payroll/PayrollApplicationTests.java
@@ -1,4 +1,4 @@
-package payroll.nonrest;
+package payroll;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class PayrollApplicationTests {
 
 	@Test
 	public void contextLoads() {
