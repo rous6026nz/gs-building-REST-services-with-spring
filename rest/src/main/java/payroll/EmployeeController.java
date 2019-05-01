be82cd2dbfd453a56dccf57c6a173fd3f6a3c559
Zaine Kingi <T826739@spark.co.nz>
2019-05-01 15:41:40 +1200

    -Convert EmployeeController get all employees and get employee by ID endpoints to RESTful resources.

--- rest/src/main/java/payroll/EmployeeController.java
+++ rest/src/main/java/payroll/EmployeeController.java
@@ -1,7 +1,12 @@
 package payroll;
 
-import java.util.List;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,15 +26,56 @@ class EmployeeController {
 
   // Aggregate root
   @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
+
+  /**
+   * Resources:
+   * A Spring HATEOAS container aimed at encapsulating collections of employee resources,
+   * and also provides the functionality to include links.
+   */
+  Resources<Resource<Employee>> all() {
+
+    /**
+     * Fetchs all the employees, but then transform them into a list of Resource<Employee> objects
+     * using the Java stream API.
+     */
+    List<Resource<Employee>> employees = repository.findAll().stream()
+      .map(employee -> new Resource<>(employee,
+        /**
+         * Builds a link to the EmployeeController's one() method, and flag it as a self link.
+         */
+        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+        /**
+         * Builds a link to the aggregate root, all(), and call it "employees".
+         */
+        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+      .collect(Collectors.toList());
+
+    return new Resources<>(employees,
+      /**
+       * Builds a link to the aggregate root top-level.
+       */
+      linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
 
-  // Get employee by ID
+
+  /**
+   * Get employee by ID:
+   * @Return Resource<Employee> - a generic container that includes not only the data but a collection of links.
+   */
   @GetMapping("/employees/{id}")
-  Employee one(@PathVariable Long id) {
-    return repository.findById(id)
+  Resource<Employee> one(@PathVariable Long id) {
+    Employee employee = repository.findById(id)
       .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+    return new Resource<>(employee,
+      /**
+       * Builds a link to the EmployeeController's one() method, and flag it as a self link.
+       */
+      linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+      /**
+       * Builds a link to the aggregate root, all(), and call it "employees".
+       */
+      linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
   }
 
   // Add new employee record
